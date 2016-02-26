ab9bf080bdae810ec06ba648f884714877a073c6
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-02-26 09:44:26 -0600

    Make plot

--- norm-seeds/seed_selection.R
+++ norm-seeds/seed_selection.R
@@ -5,9 +5,13 @@ options(stringsAsFactors = FALSE)
 odd_one_out <- read.csv("survey-1/odd_one_out.csv")
 
 odd_one_out <- filter(odd_one_out,
-                      failed_catch_trial == FALSE,
-                      problem_with_audio == FALSE)
+                      failed_catch_trial == 0,
+                      problem_with_audio == 0)
 
 ggplot(odd_one_out, aes(x = filename)) +
   geom_histogram() +
-  facet_wrap("category")
+  facet_wrap("category", scales = "free_x", ncol = 2) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  ggtitle("Odd one out")
+
+ggsave("odd_one_out.png", height = 10)
