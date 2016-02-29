d7ef1799962da4cde04f2f4e35a598116f18d7c7
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-02-29 14:28:10 -0600

    Data for second survey

--- norm-seeds/seed_selection.R
+++ norm-seeds/seed_selection.R
@@ -2,19 +2,25 @@ library(dplyr)
 library(ggplot2)
 options(stringsAsFactors = FALSE)
 
-odd_one_out <- read.csv("survey-1/odd_one_out.csv")
+count_valid_responses <- function(frame) {
+  frame <- filter(frame,
+                  failed_catch_trial == 0,
+                  problem_with_audio == 0)
+  
+  counts <- frame %>%
+    count(category, filename) %>%
+    arrange(desc(n))
+}
 
-odd_one_out <- filter(odd_one_out,
-                      failed_catch_trial == 0,
-                      problem_with_audio == 0)
+sound_similarity_6 <- read.csv("survey-1/odd_one_out.csv") %>%
+  count_valid_responses
 
 # label the two most frequently selected sounds per category
-odd_one_out_counts <- odd_one_out %>%
-  count(category, filename) %>%
-  arrange(desc(n)) %>%
+sound_similarity_6 <- sound_similarity_6 %>%
+  group_by(category) %>%
   mutate(odd_one_out = ifelse(n >= n[2], "odd", "normal"))
 
-ggplot(odd_one_out_counts, aes(x = filename, y = n)) +
+ggplot(sound_similarity_6, aes(x = filename, y = n)) +
   geom_bar(aes(fill = odd_one_out), stat = "identity") +
   facet_wrap("category", scales = "free_x", ncol = 2) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
@@ -27,3 +33,16 @@ odd_sounds <- odd_one_out_counts %>%
   select(category, filename)
 
 write.csv(odd_sounds, "survey-1/odd_sounds.csv", row.names = FALSE)
+
+# survey-2
+
+sound_similarity_4 <- read.csv("survey-2/odd_one_out.csv") %>%
+  count_valid_responses
+
+ggplot(sound_similarity_4, aes(x = filename, y = n)) +
+  geom_bar(stat = "identity") +
+  facet_wrap("category", scales = "free_x", ncol = 2) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ggtitle("Odd one out (4 per category)")
+
+ggsave("survey-2/odd_one_out.png", height = 10)
