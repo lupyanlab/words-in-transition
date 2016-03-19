5498d683da7cb17d02ae85d769fe5bc6cff94808
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-03-19 12:21:43 -0500

    Hand picked messages to transcribe

--- select_messages_to_transcribe.R
+++ select_messages_to_transcribe.R
@@ -13,37 +13,15 @@ branches <- responses %>%
   summarize(accuracy = mean(is_correct)) %>%
   ungroup()
 
-# Select the highest performing branch for each chain
-pick_best_branch <- function(chain) {
-  long_enough_branches <- chain %>%
-    group_by(first_gen_id) %>%
-    filter(max(generation) > 3) %>%
-    .$first_gen_id
-  
-  slopes <- chain %>%
-    filter(survey_type != "within", first_gen_id %in% long_enough_branches) %>%
-    group_by(first_gen_id, survey_type) %>%
-    do(mod = lm(accuracy ~ generation, data = .)) %>%
-    tidy(mod) %>%
-    filter(term == "generation")
-  
-  category_diffs <- slopes %>%
-    select(first_gen_id, survey_type, estimate) %>%
-    spread(survey_type, estimate) %>%
-    mutate(diff = between - same) %>%
-    arrange(diff)
-  
-  selected_branch_id <- category_diffs$first_gen_id[[1]]
-  
-  chain %>% filter(first_gen_id == selected_branch_id)
-}
+hand_picked <- c(566, 502, 560, 522, 466, 563, 377, 505)
 
-selected_branches <- branches %>%
-  group_by(chain_name) %>%
-  do({ pick_best_branch(.) })
+selected_branch_ids <- responses %>%
+  filter(message_id %in% hand_picked) %>%
+  .$first_gen_id %>%
+  unique
+
+selected_branches <- filter(branches, first_gen_id %in% selected_branch_ids)
 
 ggplot(selected_branches, aes(x = generation, y = accuracy)) +
-  geom_line(aes(group = survey_type, color = survey_type)) +
-  facet_wrap("chain_name")
-
-unique(selected_branches[,c("seed_id", "first_gen_id")])
+  geom_line(aes(group = survey_type, color = survey_type), stat = "summary", fun.y = "mean") +
+    facet_wrap("chain_name")
