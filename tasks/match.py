5eca457242c5cb322ae78909c36dff9f3dadefaa
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-03-20 22:05:45 -0500

    Pipeline for pulling in MTurk data'

--- tasks/match.py
+++ tasks/match.py
@@ -2,7 +2,8 @@ from invoke import task, run
 from unipath import Path
 import pandas as pd
 
-from .seeds import convert_wav_to_mp3
+from .qualtrics import Qualtrics
+from .seeds import convert_wav_to_mp3, get_creds
 
 match_transcriptions_seeds_dir = 'match-transcriptions/transcription-sources/'
 
@@ -51,3 +52,47 @@ def survey_info():
     reject_ixs = transcriptions.index[transcriptions.rejected == 1.0]
     transcriptions.drop(reject_ixs, inplace=True)
     transcriptions.to_csv('match-transcriptions/survey-1.csv')
+
+@task
+def download_qualtrics():
+    qualtrics = Qualtrics(**get_creds())
+    for survey_name in ['match_to_seed_1', 'match_to_seed_2']:
+        responses = qualtrics.get_survey_responses(survey_name)
+        responses.to_csv('match-transcriptions/qualtrics/{}.csv'.format(survey_name),
+                         index=False)
+
+
+@task
+def tidy_survey():
+    all_surveys = []
+    for survey_name in ['match_to_seed_1', 'match_to_seed_2']:
+        survey = pd.read_csv('match-transcriptions/qualtrics/{}.csv'.format(survey_name),
+                             skiprows=[0,])
+
+        loop_merge = pd.read_csv('match-transcriptions/loop_merge/{}.csv'.format(survey_name))
+
+        choice_cols = ['choice_1', 'choice_2', 'choice_3', 'choice_4']
+        choices = loop_merge[choice_cols].drop_duplicates()
+        loop_merge.drop(choice_cols, axis=1, inplace=True)
+
+        choice_map = pd.melt(choices, var_name='choice_label', value_name='url')
+        choice_map['choice'] = choice_map.choice_label.str.extract('(\d)', expand=True).astype(int)
+        choice_map['choice_filename'] = choice_map.url.apply(lambda x: Path(x).stem)
+        choice_map = choice_map[['choice', 'choice_filename']]
+        
+        id_col = 'workerId'
+
+        survey = pd.melt(survey, id_col, var_name='qualtrics_col', value_name='choice')
+        re_seed = r'^([a-z]+\-\d+)\ \((\d)\)$'
+        survey[['filename', 'row']] = survey.qualtrics_col.str.extract(re_seed, expand=True)
+        
+        survey.dropna(inplace=True)
+        survey['row'] = survey.row.astype(int)
+        survey['survey_name'] = survey_name
+        survey = survey.merge(loop_merge)
+        survey = survey.merge(choice_map)
+
+        all_surveys.append(survey)
+
+    final = pd.concat(all_surveys)
+    final.to_csv('match-transcriptions/match_transcriptions.csv', index=False)
