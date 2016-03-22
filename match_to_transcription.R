994a3b237c8bc0668711aabe2e5e742a59dc31f8
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-03-21 21:40:45 -0500

    Renamed subj id and run models

--- match_to_transcription.R
+++ match_to_transcription.R
@@ -4,21 +4,26 @@ library(ggplot2)
 
 guesses <- read.csv("match-transcriptions/match_transcriptions.csv",
                     stringsAsFactors = FALSE)
-
-extract_category <- function(x) str_split_fixed(x, "-", n = 2)[, 1]
-
-guesses$answer_category <- extract_category(guesses$filename)
-guesses$choice_category <- extract_category(guesses$choice_filename)
-guesses$is_correct <- as.numeric(guesses$answer_category == guesses$choice_category)
-
+guesses$chance <- 0.25
 
 accuracy <- guesses %>%
-  group_by(chain_name, answer_category, text) %>%
+  group_by(text_category, question_type, text) %>%
   summarize(
     accuracy = mean(is_correct),
     n = n()
   )
 
-ggplot(accuracy, aes(x = chain_name, y = accuracy)) +
+ggplot(accuracy, aes(x = text_category, y = accuracy)) +
   geom_point(shape = 1) +
-  geom_text(aes(label = text), hjust = -0.1, angle = 45)
+  geom_text(aes(label = text), hjust = -0.1, angle = 45) +
+  facet_wrap("question_type")
+
+ggplot(guesses, aes(x = 1, y = is_correct)) +
+  geom_bar(stat = "summary", fun.y = "mean") +
+  facet_wrap("question_type")
+
+summary(glm(is_correct ~ 1, offset = chance, data = guesses))
+summary(glm(is_correct ~ 1, offset = chance, data = filter(guesses, question_type == "exact")))
+summary(glm(is_correct ~ 1, offset = chance, data = filter(guesses, question_type == "category")))
+
+glmer(is_correct ~ question_type + (question_type|subj_))
--- tasks/match.py
+++ tasks/match.py
@@ -108,9 +108,9 @@ def tidy_survey():
 
     final['choice_category'] = final.choice_filename.str.split('-').str.get(0)
 
-    final.rename(columns=dict(chain_name='text_category'), inplace=True)
+    final.rename(columns=dict(workerId='subj_id', chain_name='text_category'), inplace=True)
 
-    final = final[[id_col, 'survey_name', 'text', 'text_category', 'question_type', 'choice_filename', 'choice_category']]
+    final = final[['subj_id', 'survey_name', 'text', 'text_category', 'question_type', 'choice_filename', 'choice_category']]
     final['is_correct'] = (final.text_category == final.choice_category).astype(int)
 
     final.to_csv('match-transcriptions/match_transcriptions.csv', index=False)
