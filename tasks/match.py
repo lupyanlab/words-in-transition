edf4b92d7e2b91a9f95da0131072d9012f570686
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-04-10 15:18:28 -0500

    Can recreate matches.csv

--- tasks/match.py
+++ tasks/match.py
@@ -58,10 +58,57 @@ def download():
 @task
 def tidy():
     """Compile all Qualtrics response data into a single csv."""
-    survey_names = ['match_to_seed', ]
-    survey_versions = [1, 2]
-    matches = pd.concat([tidy_survey(name, version) for name in survey_names for version in survey_versions])
-    matches.to_csv('match-transcriptions/matches.csv', index=False)
+    all_surveys = []
+    for survey_name in ['match_to_seed_1', 'match_to_seed_2']:
+        survey_csv = Path(surveys_dir, 'match_to_seed/responses/{}.csv'.format(survey_name))
+        survey = pd.read_csv(survey_csv, skiprows=[0,])
+
+        loop_merge_csv = Path(surveys_dir, 'match_to_seed/loop_merge/{}.csv'.format(survey_name))
+        loop_merge = pd.read_csv(loop_merge_csv)
+
+        choice_cols = ['choice_1', 'choice_2', 'choice_3', 'choice_4']
+        choices = loop_merge[choice_cols].drop_duplicates()
+        loop_merge.drop(choice_cols, axis=1, inplace=True)
+
+        choice_map = pd.melt(choices, var_name='choice_label', value_name='url')
+        choice_map['choice'] = choice_map.choice_label.str.extract('(\d)', expand=True).astype(int)
+        choice_map['choice_filename'] = choice_map.url.apply(lambda x: Path(x).stem)
+        choice_map = choice_map[['choice', 'choice_filename']]
+
+        id_col = 'workerId'
+
+        survey = pd.melt(survey, id_col, var_name='qualtrics_col', value_name='choice')
+        re_seed = r'^([a-z]+\-\d+)\ \((\d)\)$'
+        survey[['filename', 'row']] = survey.qualtrics_col.str.extract(re_seed, expand=True)
+
+        survey.dropna(inplace=True)
+        survey['row'] = survey.row.astype(int)
+        survey['survey_name'] = survey_name
+        survey = survey.merge(loop_merge)
+        survey = survey.merge(choice_map)
+
+        # Label the question type
+        survey_info = pd.read_csv('match-transcriptions/source_info.csv')
+        survey_info['question_type'] = 'exact'
+        survey_info['question_type'] = survey_info.question_type.where(survey_info.survey_name == survey_name, 'category')
+        survey_info['filename'] = survey_info.filename.apply(lambda x: Path(x).stem)
+        survey_info = survey_info[['filename', 'question_type']]
+        survey = survey.merge(survey_info)
+
+        all_surveys.append(survey)
+
+    final = pd.concat(all_surveys)
+    final.sort_values(id_col, inplace=True)
+
+    final['seed_id'] = final.filename.str.split('-').str.get(1)
+    final['choice_category'] = final.choice_filename.str.split('-').str.get(0)
+
+    final.rename(columns=dict(workerId='subj_id', chain_name='text_category'), inplace=True)
+
+    final = final[['subj_id', 'survey_name', 'seed_id', 'text', 'text_category', 'question_type', 'choice_filename', 'choice_category']]
+    final['is_correct'] = (final.text_category == final.choice_category).astype(int)
+
+    final.to_csv('match-transcriptions/matches.csv', index=False)
 
 
 @task
