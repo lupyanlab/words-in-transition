caa754d23566384b1a02e76b7bf62c04cdb92ee2
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-05-27 12:11:04 -0500

    Label english phrases too

--- tasks/transcrip.py
+++ tasks/transcrip.py
@@ -12,9 +12,12 @@ def label_english_words():
     import enchant
     d = enchant.Dict('en_US')
 
+    def check_english(text):
+        return all([d.check(w) for w in text.split(' ')])
+
     freqs_csv = unipath.Path(transcrip_dir, 'frequencies.csv')
     transcriptions = pandas.read_csv(freqs_csv)
-    transcriptions['is_english'] = transcriptions.text.apply(d.check)
+    transcriptions['is_english'] = transcriptions.text.apply(check_english)
 
     out_csv = unipath.Path(transcrip_dir, 'frequencies-labeled.csv')
     transcriptions.to_csv(out_csv, index=False)
