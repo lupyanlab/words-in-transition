bcad675b81ed5635a8bfb02a1af80656f178b805
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-05-27 12:51:16 -0500

    Select most frequent non-english responses

--- reports/5-match-to-seed-transcriptions/select_transcriptions_to_rate.R
+++ reports/5-match-to-seed-transcriptions/select_transcriptions_to_rate.R
@@ -1,19 +1,14 @@
 library(dplyr)
 
 library(wordsintransition)
-data("transcriptions")
+data("frequencies")
 
-survey_names <- c("hand picked 1", "hand picked 1 seeds")
-frequencies <- transcriptions %>%
-  filter(!grepl("\\.wav$", chain_name),  # catch trials have files as chain names
-         transcription_survey_name %in% survey_names) %>%
-  count(transcription_survey_name, chain_name, seed_id, message_id, text) %>%
-  arrange(desc(n)) %>%
-  mutate(order = 1:n())
-
-# Select the 4 most frequent transcriptions
 selected <- frequencies %>%
-  group_by(transcription_survey_name, message_id) %>%
+  filter(is_english == 0) %>%
+  group_by(chain_name, seed_id, message_id) %>%
+  arrange(desc(n)) %>%
+  mutate(order = 1:n()) %>%
+  ungroup() %>%
   filter(order < 5)
 
 write.csv(selected, "reports/5-match-to-seed-transcriptions/selected.csv", row.names = FALSE)
