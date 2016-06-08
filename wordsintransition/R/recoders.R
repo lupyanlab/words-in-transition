3bff57e78508d7022d5fa52ad9fe09519ef2b031
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-06-07 20:10:38 -0500

    Moved recoders to R pkg

---
+++ wordsintransition/R/recoders.R
@@ -0,0 +1,44 @@
+#' @import dplyr
+#' @export
+recode_question_type <- function(frame) {
+  map <- data_frame(
+    question_type = c("exact", "category"),
+    question_c = c(-0.5, 0.5)
+  )
+
+  frame %>% left_join(map)
+}
+
+
+#' @import dplyr
+#' @import magrittr
+#' @export
+recode_message_type <- function(frame) {
+  try(frame %<>% mutate(message_type = ifelse(message_id == seed_id, "sound_effect", "imitation")))
+
+  levels <- c("sound_effect", "imitation") %>% rev
+  labels <- c("Sound effect transcription", "Imitation transcription") %>% rev
+
+  map <- data_frame(
+    message_type = levels,
+    message_label = factor(message_type, levels = levels, labels = labels),
+    message_c = c(-0.5, 0.5)
+  )
+
+  frame %>% left_join(map)
+}
+
+
+#' @import dplyr
+#' @export
+recode_version <- function(frame) {
+  levels <- c("pilot", "A")
+  labels <- c("Pilot (Qualtrics)", "Test (App)")
+
+  map <- data_frame(
+    version = levels,
+    version_label = factor(levels, levels = levels, labels = labels)
+  )
+
+  frame %>% left_join(map)
+}
