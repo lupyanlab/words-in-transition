137c6b7b6b67ee32cee08d8766e0d3a83190393b
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-08-26 09:10:06 -0500

    Refactor main report

--- reports/5-transcription-matches/results.R
+++ reports/5-transcription-matches/results.R
@@ -1,4 +1,4 @@
-# ---- setup
+# ---- 5-setup
 library(dplyr)
 library(magrittr)
 library(stringr)
@@ -16,23 +16,16 @@ transcription_matches %<>%
   recode_message_type %>%
   recode_version
 
-# ---- selected-transcriptions
-transcription_matches %>%
-  filter(question_type != "catch_trial", version != "pilot") %>%
-  group_by(word_category, message_type, message_id) %>%
-  summarize(num_words = length(unique(word))) %>%
-  ungroup %>%
-  arrange(word_category, desc(message_type), message_id)
-
-# ---- data
 bad_subj_ids <- transcription_matches %>%
   filter(question_type == "catch_trial", is_correct == 0) %>%
   .$subj_id %>% unique
 
+catch_trials <- transcription_matches %>%
+  filter(question_type == "catch_trial")
+
 transcription_matches <- transcription_matches %>%
   filter(question_type != "catch_trial", !(subj_id %in% bad_subj_ids))
 
-# ---- plot-template
 scale_x_question <- scale_x_continuous("Question type", breaks = c(-0.5, 0.5), labels = c("Match to exact sound", "Match to same category"))
 scale_y_accuracy <- scale_y_continuous("Match to seed accuracy", labels = percent,
                                        breaks = c(0, 1, by = 0.25))
@@ -45,7 +38,20 @@ gg <- ggplot(transcription_matches, aes(x = question_c, y = is_correct)) +
   theme_minimal(base_size = 12) +
   theme(axis.ticks = element_blank())
 
-# ---- plot-means
+# ---- 5-catch-trials
+exclusions <- catch_trials %>%
+  mutate(is_correct_f = factor(is_correct, labels = c("Failed", "Passed"))) %>%
+  group_by(version, is_correct_f) %>%
+  summarize(n = length(unique(subj_id)))
+
+label_bump <- 4
+
+ggplot(exclusions, aes(x = is_correct_f, y = n)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label = n, y = ifelse(is_correct_f == "Failed", n + label_bump, n - label_bump))) +
+  scale_x_discrete("")
+
+# ---- 5-plot-means
 means_plot <- gg + geom_bar(stat = "summary", fun.y = "mean",
                             width = 0.99, alpha = 0.6)
 means_plot + ggtitle("Match accuracy by type of match")
@@ -56,12 +62,12 @@ means_plot +
   facet_grid(version_label ~ message_label) +
   ggtitle("Match accuracy over experiment versions")
 
-# ---- model
+# ---- 5-model
 acc_mod <- glmer(is_correct ~ question_c * message_c + (question_c * question_c|subj_id),
                  family = binomial, data = transcription_matches)
 tidy(acc_mod, effects = "fixed")
 
-# ---- model-preds
+# ---- 5-model-preds
 x_preds <- expand.grid(question_c = c(-0.5, 0.5), message_c = c(-0.5, 0.5))
 y_preds <- predictSE(acc_mod, x_preds, se = TRUE)
 preds <- cbind(x_preds, y_preds) %>%
