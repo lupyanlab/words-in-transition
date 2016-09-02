1afce6b4f7ba04fd0a2602b58a499f1095063b40
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-09-02 08:37:23 -0500

    Recreate version_b and version_c, but a little off

---
+++ experiments/5-transcription-matches/3_select_version_b_transcriptions_to_match.R
@@ -0,0 +1,32 @@
+library(dplyr)
+
+library(wordsintransition)
+data("transcription_frequencies")
+
+selected <- transcription_frequencies %>%
+  filter(
+    is_english == 0,
+    transcription_survey_name == "remaining-proximal-and-distal-1"
+  ) %>%
+  group_by(chain_name, seed_id, message_id) %>%
+  arrange(desc(n)) %>%
+  mutate(order = 1:n()) %>%
+  ungroup() %>%
+  filter(order < 5) %>%
+  select(
+    transcription_survey_name,
+    chain_name,
+    seed_id,
+    message_id,
+    text,
+    n,
+    order
+  ) %>%
+  arrange(
+    chain_name,
+    seed_id
+  )
+
+write.csv(selected,
+          "experiments/5-transcription-matches/surveys/version_b/selected_transcriptions.csv",
+          row.names = FALSE)
