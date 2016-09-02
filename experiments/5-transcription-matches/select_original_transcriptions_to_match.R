22e6a6fb835e4268564a314b096a20851e4d44b6
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-09-02 08:22:53 -0500

    Recreate pilot survey transcriptions to match

--- experiments/5-transcription-matches/select_original_transcriptions_to_match.R
+++ experiments/5-transcription-matches/select_original_transcriptions_to_match.R
@@ -4,11 +4,16 @@ library(wordsintransition)
 data("transcription_frequencies")
 
 selected <- transcription_frequencies %>%
-  filter(is_english == 0) %>%
+  filter(
+    is_english == 0,
+    transcription_survey_name == "hand picked 1"
+  ) %>%
   group_by(chain_name, seed_id, message_id) %>%
   arrange(desc(n)) %>%
   mutate(order = 1:n()) %>%
   ungroup() %>%
   filter(order < 5)
 
-write.csv(selected, "experiments/5-transcription-matches/selected.csv", row.names = FALSE)
+write.csv(selected,
+          "experiments/5-transcription-matches/surveys/qualtrics/transcriptions/selected_transcriptions.csv",
+          row.names = FALSE)
