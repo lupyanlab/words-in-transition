80ed2978c03e85108fe8c829445fbd1a9447c647
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-09-08 14:46:54 -0500

    Calculate distance to most frequent transcription

--- tasks/test_transcription_distance.py
+++ tasks/test_transcription_distance.py
@@ -3,9 +3,9 @@ from .transcription_distances import summarize_transcription_distance
 
 def test_transcription_distance_output():
     transcriptions = pandas.DataFrame(dict(
-        transcriptions = ['a', 'aa'],
-        message_id = [1, 1],
+        text = ['a', 'a', 'aa'],
+        message_id = [1, 1, 1],
     ))
     distances = summarize_transcription_distance(transcriptions)
-    expected_cols = ['message_id', 'distance']
+    expected_cols = ['message_id', 'no_freq', 'most_freq', 'text', 'distance']
     assert all([exp in distances for exp in expected_cols])
--- tasks/transcription_distances.py
+++ tasks/transcription_distances.py
@@ -1,2 +1,32 @@
+from difflib import SequenceMatcher
+
+
 def summarize_transcription_distance(transcriptions):
-    return transcriptions
+    distances = (transcriptions.groupby('message_id')
+                               .apply(distance_to_most_frequent))
+
+    return distances
+
+
+def distance_to_most_frequent(transcriptions):
+    """Calculate distance to the most frequent transcription."""
+    output = ['message_id', 'no_freq', 'most_freq', 'text', 'distance']
+
+    frequencies = transcriptions.text.value_counts()
+    most_frequent = frequencies.index[0]
+    most_frequent_frequency = frequencies.iloc[0]
+
+    # Label the transcriptions for which there was no agreement
+    transcriptions['no_freq'] = int(most_frequent_frequency == 1)
+
+    transcriptions['most_freq'] = most_frequent
+    matcher = SequenceMatcher()
+    matcher.set_seq1(most_frequent)
+
+    def distancer(seq2):
+        matcher.set_seq2(seq2)
+        return matcher.ratio()
+
+    transcriptions['distance'] = transcriptions.text.apply(distancer)
+
+    return transcriptions[output]
