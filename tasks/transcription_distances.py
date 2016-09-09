fc7cb72d26394b9de578891338708d8dc36aba21
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-09-08 21:31:24 -0500

    Add additional distance metrics

--- tasks/transcription_distances.py
+++ tasks/transcription_distances.py
@@ -12,7 +12,7 @@ def summarize_transcription_distance(transcriptions):
 
 def distance_to_most_frequent(transcriptions):
     """Calculate distance to the most frequent transcription."""
-    output = 'message_id text no_freq most_freq distance match length'.split()
+    output = 'message_id text no_freq most_freq distance substr length'.split()
 
     frequencies = transcriptions.text.value_counts()
     most_frequent = frequencies.index[0]
@@ -25,7 +25,7 @@ def distance_to_most_frequent(transcriptions):
     matcher = SequenceMatcher()
     matcher.set_seq1(most_frequent)
 
-    distancer_vars = 'distance match length'.split()
+    distancer_vars = 'distance substr length'.split()
 
     def distancer(seq2):
         matcher.set_seq2(seq2)
@@ -35,7 +35,7 @@ def distance_to_most_frequent(transcriptions):
         length = match.size
         substr = matcher.a[match.a:match.a+length]
 
-        return pandas.Series([distance, match, length], index=distancer_vars)
+        return pandas.Series([distance, substr, length], index=distancer_vars)
 
     transcriptions = transcriptions.join(transcriptions.text.apply(distancer))
     return transcriptions[output]
