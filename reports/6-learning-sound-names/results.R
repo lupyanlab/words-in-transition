984aa01d322a197b36bc8223d473b03f24ba9a27
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-11-18 10:22:59 -0600

    Show plot of errors by block

--- reports/6-learning-sound-names/results.R
+++ reports/6-learning-sound-names/results.R
@@ -15,19 +15,36 @@ recode_word_type <- . %>%
 
 data("learning_sound_names")
 learning_sound_names %<>%
-  mutate(rt = ifelse(is_correct == 1, rt, NA)) %>%
+  mutate(rt = ifelse(is_correct == 1, rt, NA),
+         is_error = 1 - is_correct) %>%
   mutate(word_category_by_block_ix = paste(word_category, block_ix, sep = ":")) %>%
   recode_word_type
 
 scale_x_trial_ix <- scale_x_continuous("Trial number (24 trials per block)",
                                        breaks = seq(1, 96, by = 24))
+scale_y_rt <- scale_y_continuous("Reaction time (msec)")
 
-ggbase <- ggplot(learning_sound_names, aes(trial_ix, rt)) +
-  scale_x_trial_ix +
+ggbase <- ggplot(learning_sound_names) +
   global_theme
 
+# ---- 6-errors
+ggbase_error <- ggbase
+ggbase_error$mapping <- aes(block_ix, is_error)
+
+ggbase_error +
+  geom_bar(aes(fill = factor(block_ix)), stat = "summary", fun.y = "sum") +
+  scale_x_continuous("Block number (24 trials per block)", breaks = 1:4) +
+  scale_y_continuous("Total number of errors") +
+  scale_fill_brewer(palette = "Set2") +
+  theme(legend.position = "none")
+
 # ---- 6-rts-over-trials
-ggbase +
+ggbase_rt <- ggbase +
+  scale_x_trial_ix +
+  scale_y_rt
+ggbase_rt$mapping <- aes(trial_ix, rt)
+
+ggbase_rt +
   geom_point(aes(color = factor(block_ix))) +
   geom_smooth(aes(group = factor(block_ix), color = factor(block_ix)),
               method = "lm", se = FALSE) +
@@ -36,11 +53,11 @@ ggbase +
   facet_wrap("message_label_long") +
   labs(title = "RTs over blocks by transcription type")
 
-ggbase +
+ggbase_rt +
   geom_point(aes(color = word_category)) +
   geom_smooth(aes(group = word_category_by_block_ix, color = word_category),
               method = "lm", se = FALSE) +
-  scale_color_brewer("Category", palette = "Set2") +
+  scale_color_brewer("category:", palette = "Set2") +
   theme(legend.position = "top") +
   facet_wrap("message_label_long") +
   labs(title = "RTs over blocks for each sound category")
