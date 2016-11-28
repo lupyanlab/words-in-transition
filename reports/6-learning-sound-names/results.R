64220339bcb575be7951b342d8bdf4fdba3a4a3c
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-11-28 11:14:11 -0600

    Can't believe it's working

--- reports/6-learning-sound-names/results.R
+++ reports/6-learning-sound-names/results.R
@@ -6,6 +6,8 @@ global_theme <- theme_minimal() +
 library(dplyr)
 library(magrittr)
 library(ggplot2)
+library(scales)
+library(gridExtra)
 
 library(wordsintransition)
 
@@ -22,6 +24,8 @@ learning_sound_names %<>%
 
 scale_x_trial_ix <- scale_x_continuous("Trial number (24 trials per block)",
                                        breaks = seq(1, 96, by = 24))
+scale_x_block_ix <- scale_x_continuous("Block number (24 trials per block)",
+                                       breaks = 1:4)
 scale_y_rt <- scale_y_continuous("Reaction time (msec)")
 
 ggbase <- ggplot(learning_sound_names) +
@@ -31,20 +35,15 @@ ggbase <- ggplot(learning_sound_names) +
 ggbase_error <- ggbase
 ggbase_error$mapping <- aes(block_ix, is_error)
 
-ggbase_error +
-  geom_bar(aes(fill = factor(block_ix)), stat = "summary", fun.y = "sum") +
-  scale_x_continuous("Block number (24 trials per block)", breaks = 1:4) +
-  scale_y_continuous("Total number of errors") +
-  scale_fill_brewer(palette = "Set2") +
-  theme(legend.position = "none")
-
-ggbase_error +
+error_plot <- ggbase_error +
   geom_line(aes(color = factor(message_label)),
-            stat = "summary", fun.y = "sum") +
-  scale_x_continuous("Block number (24 trials per block)", breaks = 1:4) +
-  scale_y_continuous("Total number of errors") +
+            stat = "summary", fun.y = "mean",
+            size = 1.2) +
+  scale_x_block_ix +
+  scale_y_continuous("Error rate", labels = percent) +
   scale_color_brewer("Transcription of", palette = "Set2") +
   theme(legend.position = "top")
+error_plot
 
 # ---- 6-rts-over-trials
 ggbase_rt <- ggbase +
@@ -55,17 +54,28 @@ ggbase_rt$mapping <- aes(trial_ix, rt)
 ggbase_rt +
   geom_point(aes(color = factor(block_ix)), alpha = 0.2) +
   geom_smooth(aes(group = factor(block_ix), color = factor(block_ix)),
-              method = "lm", se = FALSE) +
+              method = "loess", se = FALSE) +
   scale_color_brewer("Block", palette = "Set2") +
   theme(legend.position = "none") +
   facet_wrap("message_label_long") +
   labs(title = "RTs over blocks by transcription type")
 
-ggbase_rt +
-  geom_point(aes(color = word_category), alpha = 0.2) +
-  geom_smooth(aes(group = word_category_by_block_ix, color = word_category),
-              method = "lm", se = FALSE) +
-  scale_color_brewer("category:", palette = "Set2") +
-  theme(legend.position = "top") +
-  facet_wrap("message_label_long") +
-  labs(title = "RTs over blocks for each sound category")
+ggbase_rt_ave <- ggbase +
+  scale_x_block_ix +
+  scale_y_rt
+ggbase_rt_ave$mapping <- aes(block_ix, rt)
+
+rt_plot <- ggbase_rt_ave +
+  geom_line(aes(group = message_label, color = message_label),
+            stat = "summary", fun.y = "mean", size = 1.2) +
+  scale_color_brewer("Transcription of", palette = "Set2") +
+  theme(legend.position = "top")
+rt_plot
+
+# ---- 6-results
+first_last_gen <- filter(learning_sound_names, message_type != "sound_effect")
+grid.arrange(
+  (rt_plot %+% first_last_gen) + theme(legend.position = "none"),
+  (error_plot %+% first_last_gen) + theme(legend.position = c(0.8, 0.9)),
+  nrow = 1
+)
