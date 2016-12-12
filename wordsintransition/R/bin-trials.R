12f086cb5876a3afbb13d47d82e00d9325426697
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-12-12 09:46:39 -0600

    TDD: Trial binning works

--- wordsintransition/R/bin-trials.R
+++ wordsintransition/R/bin-trials.R
@@ -3,7 +3,13 @@
 #' @example
 #' lsn %<>% bin_trials("block_transition", before = c(30:40), after = c(1:10))
 bin_trials <- function(frame, bin_col, ...) {
+  trial_labels <- list(...)
   frame[bin_col] <- NA
+
+  for (name in names(trial_labels)) {
+    in_trial_label <- (frame$trial_ix %in% trial_labels[[name]])
+    frame[in_trial_label, bin_col] <- name
+  }
+
   frame
 }
-
--- wordsintransition/tests/testthat/test_trial_bins.R
+++ wordsintransition/tests/testthat/test_trial_bins.R
@@ -1,4 +1,5 @@
 library(wordsintransition)
+library(dplyr)
 
 context("Bin trials")
 
@@ -7,4 +8,29 @@ test_that("bin trials creates new col", {
   new_col_name <- "new_bin"
   result <- bin_trials(frame, new_col_name, first_trial = 1)
   expect_true((new_col_name %in% colnames(result)))
+})
+
+test_that("new col contains trial labels", {
+  frame <- data_frame(trial_ix = 1:50)
+  new_col_name <- "new_bin"
+  result <- bin_trials(frame, new_col_name, first_trial = 1)
+  expect_true("first_trial" %in% unique(result[[new_col_name]]))
+})
+
+test_that("trial bins are correct length", {
+  frame <- data_frame(trial_ix = 1:50)
+  new_col_name <- "new_bin"
+  test_bin <- 1:10
+  expected_bin_length <- length(1:10)
+  result <- bin_trials(frame, new_col_name, test_bin = test_bin)
+  expect_equal(sum(result[new_col_name] == "test_bin", na.rm = TRUE), expected_bin_length)
+})
+
+test_that("multiple trial bins", {
+  frame <- data_frame(trial_ix = 1:50)
+  new_col_name <- "new_bin"
+  result <- bin_trials(frame, new_col_name, first_bin = 1, last_bin = 50)
+  unique_vals <- unique(result[[new_col_name]])
+  expect_true("first_bin" %in% unique_vals)
+  expect_true("last_bin" %in% unique_vals)
 })
