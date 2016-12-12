f71161f181f1f7c463c28ce79054585420088ac9
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-12-12 09:47:59 -0600

    Export bin trials function

--- wordsintransition/R/bin-trials.R
+++ wordsintransition/R/bin-trials.R
@@ -1,7 +1,5 @@
 #' Bin trials into named groups.
-#'
-#' @example
-#' lsn %<>% bin_trials("block_transition", before = c(30:40), after = c(1:10))
+#' @export
 bin_trials <- function(frame, bin_col, ...) {
   trial_labels <- list(...)
   frame[bin_col] <- NA
