48222ce2f40d421d9a51a19ef98e4d6643b5d023
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-12-14 12:52:36 -0600

    Add getopts arg parse to build script

--- paper/_build.sh
+++ paper/_build.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+
+while getopts ":o" opt; do
+  case $opt in
+    o)
+      open _book/index.html
+      ;;
+  esac
+done
