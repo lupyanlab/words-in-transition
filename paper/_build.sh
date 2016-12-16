44ba2308f576e546fd711e4ba2d403392f70e7b8
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-12-16 08:16:53 -0600

    Put some results in each chapter

--- paper/_build.sh
+++ paper/_build.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
+open_after=false
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-
-while getopts ":o" opt; do
+while getopts ":oc" opt; do
   case $opt in
     o)
-      open _book/index.html
+      open_after=true
+      ;;
+    c)
+      rm -rf .cache/ figs/
       ;;
   esac
 done
+
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+
+if [ "$open_after" = true ]; then
+  open _book/index.html
+fi
