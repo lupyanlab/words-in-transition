770d416d2f950390b0ac157d2263b2beacdd5648
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-01-29 09:59:18 -0600

    Add function for reporting glmer models

--- wordsintransition/R/reporting-statistical-results.R
+++ wordsintransition/R/reporting-statistical-results.R
@@ -7,3 +7,16 @@ lmer_mod_results <- function(lmer_mod, param) {
     as.list()
   sprintf("_b_ = %.2f (%.2f), _t_ = %.2f", results$estimate, results$std.error, results$statistic)
 }
+
+
+#' Present the results of a hierarchical generalized linear model as in a results section.
+#' @import dplyr
+#' @export
+glmer_mod_results <- function(glmer_mod, param) {
+  results <- broom::tidy(glmer_mod, effects = "fixed") %>%
+    filter(term == param) %>%
+    as.list()
+  results["odds"] <- log(results$estimate)
+  sprintf("_b_ = %.2f (%.2f) log-odds, odds = %.2f, _z_ = %.2f, _p_ = %.2f",
+          results$estimate, results$std.error, results$odds, results$statistic, results$p.value)
+}
