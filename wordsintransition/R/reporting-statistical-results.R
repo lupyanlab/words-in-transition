2fd2ada2f7c7d61a53b5e643645bbe1454df14e0
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-01-29 11:59:07 -0600

    Add switch for including/excluding odds conversions

--- wordsintransition/R/reporting-statistical-results.R
+++ wordsintransition/R/reporting-statistical-results.R
@@ -12,11 +12,17 @@ lmer_mod_results <- function(lmer_mod, param) {
 #' Present the results of a hierarchical generalized linear model as in a results section.
 #' @import dplyr
 #' @export
-glmer_mod_results <- function(glmer_mod, param) {
+glmer_mod_results <- function(glmer_mod, param, odds = FALSE) {
   results <- broom::tidy(glmer_mod, effects = "fixed") %>%
     filter(term == param) %>%
     as.list()
   results["odds"] <- log(results$estimate)
-  sprintf("_b_ = %.2f (%.2f) log-odds, odds = %.2f, _z_ = %.2f, _p_ = %.2f",
-          results$estimate, results$std.error, results$odds, results$statistic, results$p.value)
+  if (odds == TRUE) {
+    formatted = sprintf("_b_ = %.2f (%.2f) log-odds, odds = %.2f, _z_ = %.2f, _p_ = %.2f",
+                        results$estimate, results$std.error, results$odds, results$statistic, results$p.value)
+  } else {
+    formatted = sprintf("_b_ = %.2f (%.2f) log-odds, _z_ = %.2f, _p_ = %.2f",
+                        results$estimate, results$std.error, results$statistic, results$p.value)
+  }
+  formatted
 }
