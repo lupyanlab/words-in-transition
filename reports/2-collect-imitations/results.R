70ba0dadadd3b0fd24bce01477d836802aa64c67
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-03-20 15:39:42 -0500

    Removed redundant functions

--- reports/2-collect-imitations/results.R
+++ reports/2-collect-imitations/results.R
@@ -10,27 +10,6 @@ library(AICcmodavg)
 library(wordsintransition)
 data("acoustic_similarity_judgments")
 
-z_score_by_subj <- function(frame) {
-  z_score <- function(x) (x - mean(x, na.rm = TRUE)) / sd(x, na.rm = TRUE)
-  frame %>%
-    group_by(name) %>%
-    mutate(similarity_z = z_score(similarity)) %>%
-    ungroup()
-}
-
-recode_edge_generations <- function(frame) {
-  data_frame(
-    edge_generations = c("1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8"),
-    edge_generation_n = seq_along(edge_generations)
-  ) %>%
-    left_join(frame, .)
-}
-
-determine_trial_id <- function(frame) {
-  # Warning! Ignores presentation order.
-  mutate(frame, trial_id = paste(sound_x, ":", sound_y))
-}
-
 acoustic_similarity_judgments %<>%
   mutate(similarity = ifelse(similarity == -1, NA, similarity)) %>%
   z_score_by_subj() %>%
