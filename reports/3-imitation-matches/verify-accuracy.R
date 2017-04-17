1353e4fdc8bd3540e7c6ec66c9c64d5c01150fb0
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-04-17 07:27:20 -0500

    Redo analyses without bad questions

--- reports/3-imitation-matches/verify-accuracy.R
+++ reports/3-imitation-matches/verify-accuracy.R
@@ -32,6 +32,7 @@ ggplot(bad_questions) +
   aes(survey_type) +
   geom_bar()
 
+# Ensure reproducibility
 data("imitation_matches")
 bad_question_ids <- imitation_matches %>%
   filter(question_type != "catch_trial") %>%
@@ -39,5 +40,28 @@ bad_question_ids <- imitation_matches %>%
   summarize(accuracy = mean(is_correct)) %>%
   filter(accuracy == 0) %>%
   .$question_pk
-
 setequal(bad_questions$question_pk, bad_question_ids)
+
+bad_questions <- imitation_matches %>%
+  filter(question_pk %in% bad_question_ids)
+
+ggplot(bad_questions) +
+  aes(factor(selection)) +
+  geom_bar() +
+  facet_wrap("question_pk", scales = "free_x")
+
+unique_options_per_bad_question <- bad_questions %>%
+  group_by(question_pk) %>%
+  summarize(n_unique_options = length(unique(selection)))
+
+ggplot(unique_options_per_bad_question) +
+  aes(factor(n_unique_options)) +
+  geom_bar()
+
+bad_questions_with_agreement <- unique_options_per_bad_question %>%
+  filter(n_unique_options == 1) %>%
+  left_join(bad_questions)
+
+ggplot(bad_questions_with_agreement) +
+  aes(factor(question_pk)) +
+  geom_bar()
